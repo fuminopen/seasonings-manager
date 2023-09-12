989783553d033e590085b76eca29cfbe60eec4d6
fuminopen <fuminopen@gmail.com>
2023-09-13 05:32:55 +0900

    add: 残量VOは0~100の間のテスト追加

---
+++ tests/Unit/ValueObjects/RemainingTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit\ValueObjects;
+
+use PHPUnit\Framework\TestCase;
+
+class RemainingTest extends TestCase
+{
+    /**
+     * 残量は0 ~ 100の間
+     *
+     * @test
+     * @dataProvider invalidRemainingProvider
+     */
+    public function remainingMustBeBetween0To100(int $remaining): void
+    {
+        $this->expectException(InvalidRemainingException::class);
+
+        new Remaining($remaining);
+    }
+
+    /**
+     * 不正な残量
+     */
+    public static function invalidRemainingProvider(): array
+    {
+        return [
+            [-1,],
+            [101,],
+        ];
+    }
+}
