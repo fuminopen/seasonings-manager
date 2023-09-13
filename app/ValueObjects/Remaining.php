ed8242ddd42ae2cd15d03c333864dd56ac21734d
fuminopen <fuminopen@gmail.com>
2023-09-14 05:36:27 +0900

    add: 最大量で新しいインスタンスを作成するメソッドをRemainingに追加

--- app/ValueObjects/Remaining.php
+++ app/ValueObjects/Remaining.php
@@ -4,12 +4,35 @@ namespace App\ValueObjects;
 
 use App\Exceptions\ValueObjects\InvalidRemainingException;
 
+/**
+ * @property-read int $value
+ */
 final class Remaining
 {
+    /**
+     * 最小量
+     */
+    private const MIN = 0;
+
+    /**
+     * 最大量
+     */
+    private const MAX = 100;
+
     public function __construct(public readonly int $value)
     {
-        if ($value > 100 || $value < 0) {
+        if ($value > self::MAX || $value < self::MIN) {
             throw new InvalidRemainingException();
         }
+    }
+
+    /**
+     * 最大量で新しいインスタンスを作成する
+     *
+     * @return self
+     */
+    public static function createAsFull(): self
+    {
+        return new self(self::MAX);
     }
 }
