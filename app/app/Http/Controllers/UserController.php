59a1037f0997e0f50f9a9eba663778a17090e10b
fuminopen <fuminopen@gmail.com>
2023-09-17 15:57:44 +0900

    add: 新規ユーザー作成リクエスト時にDBにもレコードを追加する実装作成

--- app/app/Http/Controllers/UserController.php
+++ app/app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
@@ -15,11 +16,22 @@ class UserController extends Controller
      */
     public function create(Request $request): JsonResponse
     {
-        User::createNew(
+        $user = User::createNew(
             $request->name,
             $request->email,
             $request->password
         );
+
+        if (! $user->save()) {
+            return response()->json(
+                [
+                    'message' => '新規ユーザーの登録に失敗しました。',
+                    'error' => 'user.create.unexpected_error',
+                    'data' => [],
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
 
         return response()->json([
             'message' => '新規ユーザーの登録が完了しました。',
