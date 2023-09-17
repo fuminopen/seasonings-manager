86ad4034382d9a9d46dea73f6ca49cc281c6ed5e
fuminopen <fuminopen@gmail.com>
2023-09-17 15:29:20 +0900

    add: ユーザーコントローラーにユーザー作成のリクエストのテスト

---
+++ app/tests/Feature/Http/Controllers/UserControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    /**
+     * @test
+     */
+    public function canCreateNewUser(): void
+    {
+        $response = $this->post(
+            '/user',
+            [
+                'name' => $this->faker->userName(),
+                'email' => $this->faker->safeEmail(),
+                'password' => $this->faker->password(),
+            ]
+        );
+
+        $response->assertStatus(200);
+    }
+}
