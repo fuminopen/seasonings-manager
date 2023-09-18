6f2e8f3bbccbe828d3dc2c2b4e64bda6308107bf
fuminopen <fuminopen@gmail.com>
2023-09-18 11:19:35 +0900

    add: ユーザーが管理者であることを判定するメソッドと、管理者としてユーザーを作成する実装追加

--- app/app/Models/UserGroup.php
+++ app/app/Models/UserGroup.php
@@ -10,17 +10,48 @@ class UserGroup extends Model
     use HasFactory;
 
     /**
-     * 新しいインスタンスを作成する
+     * 管理者として新しいインスタンスを作成する
      *
      * @param int $userId
      * @param int $groupId
      * @return self
      */
-    public static function createNew(int $userId, int $groupId): self
+    public static function createNewAsAdmin(int $userId, int $groupId): self
     {
+        return self::createNew(
+            $userId,
+            $groupId,
+            config('const.db.seasonings.user_types.admin.id')
+        );
+    }
+
+    /**
+     * 新しいインスタンスを作成する
+     *
+     * @param int $userId
+     * @param int $groupId
+     * @param int $userTypeId
+     * @return self
+     */
+    private static function createNew(
+        int $userId,
+        int $groupId,
+        int $userTypeId,
+    ): self {
         return new self([
             'user_id' => $userId,
             'group_id' => $groupId,
+            'user_type_id' => $userTypeId,
         ]);
+    }
+
+    /**
+     * 管理者である
+     *
+     * @return bool
+     */
+    public function isAdmin(): bool
+    {
+        return $this->user_type_id === config('const.db.seasonings.user_types.admin.id');
     }
 }
---
+++ app/config/const.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'db' => [
+        'seasonings' => [
+            'user_types' => [
+                'admin' => [
+                    'id' => 1,
+                    'name' => '管理者',
+                ]
+            ],
+        ],
+    ],
+];
