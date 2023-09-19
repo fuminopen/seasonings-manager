6a8546dd101725af58bb41510ad84ed101b17c9f
fuminopen <fuminopen@gmail.com>
2023-09-19 22:07:05 +0900

    add: UserがGroupを新規作成する際にDBに対して適切にレコードを保持できることのテストを追加

---
+++ app/tests/Feature/Models/UserTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * グループ新規作成時にDBに対して適切にレコードを作成することができる
+     *
+     * @test
+     */
+    public function canSaveGroupOnDb(): void
+    {
+        /**
+         * 準備
+         */
+        $user = User::factory()->create();
+
+        /**
+         * 実行
+         */
+        $group = $user->createGroup('グループ名');
+
+        /**
+         * 検証
+         */
+        $this->assertSame(
+            'グループ名',
+            $group->name,
+        );
+
+        $this->assertDatabaseHas(
+            'groups',
+            [
+                'id' => $group->id,
+            ]
+        );
+        $this->assertDatabaseHas(
+            'user_groups',
+            [
+                'user_id' => $user->id,
+                'group_id' => $group->id,
+            ]
+        );
+    }
+}
