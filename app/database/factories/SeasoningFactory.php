05a6cd441465dd989a67a44f799ff437402bafb4
fuminopen <fuminopen@gmail.com>
2023-09-23 22:10:31 +0900

    add: グループの調味料をリストで取得するAPIのテスト作成

---
+++ app/database/factories/SeasoningFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Group;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Seasoning>
+ */
+class SeasoningFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word(),
+            'group_id' => Group::factory(),
+            'remaining' => random_int(0, 100),
+        ];
+    }
+}
--- app/tests/Feature/Http/Controllers/SeasoningControllerTest.php
+++ app/tests/Feature/Http/Controllers/SeasoningControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Models\Group;
+use App\Models\Seasoning;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -38,5 +40,40 @@ class SeasoningControllerTest extends TestCase
                 'name' => $seasoningName,
             ]
         );
+    }
+
+    /**
+     * 調味料を取得できる
+     *
+     * @test
+     */
+    public function canGetSeasonings(): void
+    {
+        $group = Group::factory()->create();
+
+        $seasonings = Seasoning::factory(3)
+            ->recycle($group)
+            ->create();
+
+        $response = $this->get('/seasonings', [
+            'group_id' => $group->id,
+        ]);
+
+        $response->assertOk();
+
+        $result = $response->json('data');
+
+        $this->assertSame(
+            3,
+            count($result)
+        );
+
+        foreach ($result as $r) {
+            $this->assertTrue(
+                $seasonings->filter(
+                    fn (Seasoning $seasoning) => $seasoning->id === $r->id
+                )->isNotEmpty()
+            );
+        }
     }
 }
