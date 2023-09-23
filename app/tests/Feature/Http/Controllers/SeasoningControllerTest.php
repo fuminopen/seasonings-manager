8f44dbe69a806adecd610bad2bb275a9056f32dc
fuminopen <fuminopen@gmail.com>
2023-09-23 21:34:53 +0900

    add: 調味料を作成するコントローラーのテストを作成

---
+++ app/tests/Feature/Http/Controllers/SeasoningControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SeasoningControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    /**
+     * 新規調味料を作成できる
+     *
+     * @test
+     */
+    public function canCreateSeasonings(): void
+    {
+        $this->seed();
+
+        $user = User::factory()->create();
+        $group = $user->createGroup($this->faker->company());
+
+        $seasoningName = $this->faker->word();
+
+        $response = $this->post('/seasonings', [
+            'user_id' => $user->id,
+            'group_id' => $group->id,
+            'name' => $seasoningName,
+        ])->assertOk();
+    }
+}
